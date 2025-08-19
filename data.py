25aa0cf835199ac4559ae2e789546c14f82de093
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 14:11:04 +0800

    feat: 默认温度和topp趋近于0, 修复apibug

--- data.py
+++ data.py
@@ -9,8 +9,8 @@ class TranslateReq:
     presence_penalty: float = 0.0
     frequency_penalty: float = 0.0
     repetition_penalty: float = 1.0
-    temperature: float = 1.0
-    top_p: float = 1.0
+    temperature: float = .0
+    top_p: float = 1e-6
     top_k: int = -1
     min_p: float = 0.0
     seed: int = None
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ translator_semaphore = Semaphore(3)
 app = FastAPI()
 
 
-@app.post('v1/translate')
+@app.post('/v1/translate')
 async def _translate(req: TranslateReq):
     async with translator_semaphore:
         return translate(
