3b6e84c347045bad68888cd1f080082b615f7e92
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 13:41:42 +0800

    feat: 实现 seed-x 翻译接口

---
+++ data.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TranslateReq:
+    sentence: str
+    target_lang: str
+    resample: int = 1
+    presence_penalty: float = 0.0
+    frequency_penalty: float = 0.0
+    repetition_penalty: float = 1.0
+    temperature: float = 1.0
+    top_p: float = 1.0
+    top_k: int = -1
+    min_p: float = 0.0
+    seed: int = None
+    max_tokens: int = 4096
+    min_tokens: int = 0
