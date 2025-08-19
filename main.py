baea0e4f8844edebd874c93c0424d289a4b0ff87
吴子豪 <vortez.wohl@gmail.com>
2025-08-19 17:31:07 +0800

    feat: 支持直接获取思维链

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from asyncio import Semaphore
 
 from fastapi import FastAPI
-from llm_inference.translation.seed_x import translate
+from llm_inference.translation.seed_x import translate, translate_cot
 
 from data import TranslateReq
 
@@ -12,7 +12,29 @@ app = FastAPI()
 @app.post('/v1/translate')
 async def _translate(req: TranslateReq):
     async with translator_semaphore:
-        translation = translate(
+        cot = translate(
+            sentence=req.sentence,
+            target_lang=req.target_lang,
+            presence_penalty=req.presence_penalty,
+            frequency_penalty=req.frequency_penalty,
+            repetition_penalty=req.repetition_penalty,
+            temperature=req.temperature,
+            top_p=req.top_p,
+            top_k=req.top_k,
+            min_p=req.min_p,
+            seed=req.seed,
+            max_tokens=req.max_tokens,
+            min_tokens=req.min_tokens
+        )
+        return {'original': req.sentence,
+                'lang': req.target_lang,
+                'cot': cot}
+
+
+@app.post('/v1/translate')
+async def _translate(req: TranslateReq):
+    async with translator_semaphore:
+        translation = translate_cot(
             sentence=req.sentence,
             target_lang=req.target_lang,
             resample=req.resample,
